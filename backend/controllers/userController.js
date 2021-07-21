1efc2ea3f1951331b0e3b880fbed869d9955b28e
Ragul333 <63944210+Ragul333@users.noreply.github.com>
2021-07-21 17:59:52 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,7 @@ const sendgridTransport = require('nodemailer-sendgrid-transport')
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
-        api_key:"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"
+        api_key:""
     }
 }))
 
